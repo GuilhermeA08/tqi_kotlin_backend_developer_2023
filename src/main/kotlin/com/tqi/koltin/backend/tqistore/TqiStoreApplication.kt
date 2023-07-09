8631b0096910709ce4646a3f181ee3b781f9c238
Guilherme Augusto <guilhermesaldanha08@gmail.com>
2023-07-09 20:36:12 -0300

    initial commit

---
+++ src/main/kotlin/com/tqi/koltin/backend/tqistore/TqiStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.tqi.koltin.backend.tqistore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TqiStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<TqiStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/tqi/koltin/backend/tqistore/TqiStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tqi.koltin.backend.tqistore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TqiStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
